b540d2a39d3e117f3e87af80855ce00be87fee2c
we hkawng <ngaigadai@gmail.com>
2020-06-25 21:12:27 +0700

    update 25June

--- L2prac_app/urls.py
+++ L2prac_app/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from L2prac_app import views
 
+#Template tagging
+
+app_name = 'L2prac_app'
+
 urlpatterns = [
-    url(r'^$',views.users,name='users'),
+    url(r'^$',views.home,name='home'),
+    url(r'^users/$',views.users,name='users'),
+    url(r'^form/$',views.formUser,name='formUser'),
 ]
--- level2_project/urls.py
+++ level2_project/urls.py
@@ -20,6 +20,6 @@ from L2prac_app import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('admin/', admin.site.urls),
-    path('users/',include('L2prac_app.urls')),
+    path('L2prac_app/',include('L2prac_app.urls')),
     path('form/',views.formUser,name='formUser'),
 ]
